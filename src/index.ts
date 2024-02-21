be1a7be3d4bdcc7dec852a7853880597dbfed0aa
Alice Pote <alice.writes.wrongs@gmail.com>
2024-02-21 16:46:06 -0500

    ok basic structure

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-  auth: 'YOUR-TOKEN'
-})
+import { Octokit } from "@octokit/rest";
 
-await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
-  owner: 'OWNER',
-  repo: 'REPO',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
+async function main() {
+  const octokit = new Octokit({
+    auth: "YOUR-TOKEN",
+  });
+
+  const resp = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
+    owner: "OWNER",
+    repo: "REPO",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    fetch,
+  });
+
+  console.log(resp);
+}
+
+main();
